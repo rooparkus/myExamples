7dca72444cb70c5bb550a30fe3c46f5cb5643f61
Roopa Rkus <roopa.a@ruckuswireless.com>
2015-03-18 16:14:31 +0530

    my examples to leanr explore and refresh

---
+++ src/com/test/java/system/GetHeapSize.java
@@ -0,0 +1,12 @@
+package com.test.java.system;
+
+public class GetHeapSize {
+    public static void main(String[]args){
+
+        //Get the jvm heap size.
+        long heapSize = Runtime.getRuntime().totalMemory();
+
+        //Print the jvm heap size.
+        System.out.println("Heap Size = " + heapSize);
+    }
+}
